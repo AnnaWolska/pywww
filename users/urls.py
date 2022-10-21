fab6079fb52cdf87c0ee4cbaec53a74e38439110
AnnaWolska <a_wl@wp.pl>
2022-10-21 22:40:30 +0200

    2

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import user_counter
+from .views import user_counter, user_detail_view, user_list_view, user_update_view
+from django.views.generic import TemplateView
+from django.views import defaults as defaults_views
+
 
 app_name = "users"
 urlpatterns = [
     path('~count', view=user_counter, name="counter"),
+    path("sometemplate",TemplateView.as_view(template_name="sometemplate.html"),name="sometemplate"),
+    path("<str:username>/", view=user_detail_view, name="detail"),
     ]
--- users/views.py
+++ users/views.py
@@ -1,7 +1,11 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
 from django.contrib.auth.models import User
+from django.views.generic import DetailView, UpdateView
+from django.views.generic import ListView
+from django.views.generic import TemplateView
 
 
 class UserCounter(View):
@@ -15,3 +19,41 @@ class UserCounter(View):
 
 user_counter = UserCounter.as_view()
 
+
+# class MyDetailView(DetailView):
+#     model = User
+
+
+# class MyDetailView(LoginRequiredMixin, DetailView):
+#     model = User
+#     slug_field = "username"
+#     slug_url_kwarg = "username"
+
+class MyDetailView(LoginRequiredMixin, DetailView):
+    model = User
+    slug_field = "username"
+    slug_url_kwarg = "username"
+
+
+user_detail_view = MyDetailView.as_view()
+
+
+# class MyListView(DetailView):
+#     model = User
+
+
+class UserListView(ListView):
+    model = User
+
+
+user_list_view = UserListView.as_view()
+
+
+class UserUpdateView(LoginRequiredMixin, UpdateView):
+    fields = [
+        "name",
+        "bio",
+    ]
+
+
+user_update_view = UserUpdateView.as_view()
